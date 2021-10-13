904f1a6a78d64f7db77fe78e3e17e117c299bd86
Metin NALCI <metinnalci@outlook.com>
2021-10-14 02:55:51 +0300

    Add project files.

---
+++ Repository/ICrudRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSD.Util.Data.Repository
+{
+    public interface ICrudRepository<T, ID> where T : class
+    {
+        long Count();
+        void Delete(T entity);
+        void DeleteAll();
+        void DeleteAll(IEnumerable<T> entities);
+        void DeleteAllById(IEnumerable<ID> ids);
+        void DeleteById(ID id);
+        bool ExistById(ID id);
+        IEnumerable<T> FindAll();
+        T FindById(ID id);
+        T Save(T entity);
+        IEnumerable<T> SaveAll(IEnumerable<T> entities);
+    }
+}
---
+++ Service/DataServiceException.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSD.Util.Data.Service
+{   
+    public class DataServiceException : Exception
+    {
+        public DataServiceException(string message, Exception innerException)
+            : base(message, innerException)
+        { }
+            
+        public override string Message
+        {
+            get
+            {
+
+                var msg = InnerException != null ? ", InnerException Message" + InnerException.Message : "";
+
+                return $"Message:{base.Message}{msg}";
+
+            }
+        }
+    }
+}
